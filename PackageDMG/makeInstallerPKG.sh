71277995e2f68c491453df56eda958debc68d91f
Asuka Saito <asuka.saito1@jp.ricoh.com>
2024-03-14 18:20:02 +0900

    NA_O365 パスワード暗号化、鍵開け対応版、1.7.0.0

--- PackageDMG/makeInstallerPKG.sh
+++ PackageDMG/makeInstallerPKG.sh
@@ -22,7 +22,7 @@ fi
 echo "Start."
 echo "=================================================="
 echo ""
-DMGName=Ricoh\ PS\ Driver\ for\ mac_na_O365_V1.5.0.1
+DMGName=Ricoh\ PS\ Driver\ for\ mac_na_O365_V1.7.0.0
 SignServer=${CodeSignServer}
 OSSLicenseTextFile=OSS\ License.txt
 
